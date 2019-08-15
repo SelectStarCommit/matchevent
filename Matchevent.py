70c7d302ba4a16d6927af0fb3ca6c966aac70914
stevencericson <tetelestia@gmail.com>
2019-08-14 19:04:44 -0500

    F string variables

--- Matchevent.py
+++ Matchevent.py
@@ -5,12 +5,11 @@ def write_line(file_name, line):
 		f.write(line)
 		
 def match_update(score, min, event):
-	s = ' '
 	h, hs, v, vs = (score)
-	current_score = str(h + s + hs + s + v + s + vs)
+	current_score = f"{h} {hs} {v} {vs}"
 	time = strftime("%I:%M %p", localtime())
 	date = strftime("%Y%m%d", localtime())
-	match_line = time + ' | ' + current_score + ' | ' + min + '\': ' + event + '\n'
+	match_line = f"{time} | {current_score} | {min}\': {event}\n"
 	file_name = str(date + h + v + '.txt')
 	write_line(file_name, match_line)
 
